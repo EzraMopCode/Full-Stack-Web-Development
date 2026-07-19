3bc4d2548d26c8a126f62b8ec45fcddb45f9a384
EzraMopCode <your_email@example.com>
2026-07-19 22:31:49 +0100

    Feat: Complete Day 4 Account Class V1 with Properties and ETB Statements

--- module-01-foundation/mini-project/day04/account_mangmentSystem.py
+++ module-01-foundation/mini-project/day04/account_mangmentSystem.py
@@ -1,20 +1,47 @@
 class Account:
     def __init__(self, owner, number, balance=0):
-      self.owner = owner
-      self.account_number = number
-      self.__balance = balance
+        self.owner = owner
+        self.account_number = number
+        self.__balance = balance
 
-      @property
-      def balance(self):
+    @property
+    def balance(self):
         return self.__balance
-      def deposit(self, amount):
+
+    def deposit(self, amount):
         if amount <= 0:
-         raise ValueError("Amount must be positive")
+            raise ValueError("Deposit amount must be positive.")
         self.__balance += amount
-      def withdraw(self, amount):
-        if balance > amount:
-          self.balance -= amount
-        elif amount <= 0:
-          raise ValueError("Please Enter Valid Input")
-        else:
-            
+        print(f"Successfully deposited {amount:,.2f} ETB.")
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdrawal amount must be positive.")
+        if amount > self.__balance:
+            raise ValueError("Transaction Rejected: Insufficient funds (Overdraft).")
+
+        self.__balance -= amount
+        print(f"Successfully withdrew {amount:,.2f} ETB.")
+
+    def statement(self):
+        print("\n" + "="*35)
+        print("        ADDIS BANK STATEMENT        ")
+        print("="*35)
+        print(f"Account Owner : {self.owner}")
+        print(f"Account No.   : {self.account_number}")
+        print(f"Current Balance: {self.__balance:,.2f} ETB")
+        print("="*35 + "\n")
+
+
+
+print("Initializing Ezra's Account...")
+my_account = Account("Ezra Mesele", "ETB-987654", 1000.0)
+
+my_account.statement()
+
+my_account.deposit(500.50)
+my_account.withdraw(200.00)
+
+
+#print(f"\nChecking property balance helper: {my_account.balance} ETB")
+#my_account.statement()
