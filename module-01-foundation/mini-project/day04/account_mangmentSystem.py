7c1b843bd44f3e81b8b0af713cac4b6eb8060612
EzraMopCode <your_email@example.com>
2026-07-19 21:44:57 +0100

    Fix: Add correct OOp.py and clean up files

---
+++ module-01-foundation/mini-project/day04/account_mangmentSystem.py
@@ -0,0 +1,20 @@
+class Account:
+    def __init__(self, owner, number, balance=0):
+      self.owner = owner
+      self.account_number = number
+      self.__balance = balance
+
+      @property
+      def balance(self):
+        return self.__balance
+      def deposit(self, amount):
+        if amount <= 0:
+         raise ValueError("Amount must be positive")
+        self.__balance += amount
+      def withdraw(self, amount):
+        if balance > amount:
+          self.balance -= amount
+        elif amount <= 0:
+          raise ValueError("Please Enter Valid Input")
+        else:
+            
